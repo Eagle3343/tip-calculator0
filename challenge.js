51e8fe8d12483c60d51a1e73b8c2a31a26c06a21
Eagle3343 <Eagle3343>
2024-11-10 20:36:34 +0400

    code solved

--- challenge.js
+++ challenge.js
@@ -1,2 +1,19 @@
 // Good Luck! You got this 💪🏾
 // Write your code here.
+
+
+let bill= 275;
+
+
+if ( bill >=50 && bill <=300) {
+    console.log(`the bill is $${bill} and the tip is (15% ) $${bill*0.15}, Total to be paid: ${(bill * 0.15)+bill}`)
+}
+
+
+else if(bill >301 ){
+    console.log(`the bill is $${bill} and the tip is(20% ) $${bill*0.20} of the bill,Total to be paid :$${(bill * 0.20)+bill}`)
+}
+
+
+
+
